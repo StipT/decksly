b84b4e7074b3259ca3029f10bc36642806436c87
StipT <tostipanic@gmail.com>
2022-12-13 19:13:07 +0100

    Create reusable text styles and colors

---
+++ lib/common/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/rendering.dart';
+
+class AppColors {
+  static const Color primary = Color.fromRGBO(236, 155, 62, 1);
+  static const Color primaryFaded = Color.fromRGBO(236, 155, 62, 0.2);
+  static const Color background = Color.fromRGBO(14, 19, 36, 1);
+  static const Color text = Color.fromRGBO(228, 236, 239, 1);
+  static const Color navBarColor = Color.fromRGBO(1, 5, 16, 1);
+  static const Color splashColor = Color.fromRGBO(182, 5, 51, 0.28);
+}
---
+++ lib/common/fonts.dart
@@ -0,0 +1,54 @@
+// ignore: avoid_classes_with_only_static_members
+import 'package:decksly/common/colors.dart';
+import 'package:flutter/material.dart';
+
+// ignore: avoid_classes_with_only_static_members
+class FontStyles {
+  static TextStyle get semiBold12 => const TextStyle(
+        fontWeight: FontWeight.w600,
+        fontSize: 12,
+        color: AppColors.text,
+      );
+
+  static TextStyle semiBold15 = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 15,
+    color: AppColors.text,
+  );
+
+  static TextStyle semiBold20 = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    color: AppColors.text,
+  );
+
+  static TextStyle semiBold22 = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 22,
+    color: AppColors.text,
+  );
+
+  static TextStyle regular11 = const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 11,
+    color: AppColors.text,
+  );
+
+  static TextStyle regular12 = const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    color: AppColors.text,
+  );
+
+  static TextStyle light12 = const TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 12,
+    color: AppColors.text,
+  );
+
+  static TextStyle light13 = const TextStyle(
+    fontWeight: FontWeight.w300,
+    fontSize: 13,
+    color: AppColors.text,
+  );
+}
